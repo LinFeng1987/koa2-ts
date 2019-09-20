dec3ac10dccbfdee1e6adacafc088a6fb8d2803b
76299831 <hui199517@gmail.com>
2019-09-20 09:56:26 +0800

    no message

--- src/controller/app/home.ts
+++ src/controller/app/home.ts
@@ -1,6 +1,6 @@
 module.exports = {
     async GetHome(ctx: any, next: any) {
-        await ctx.db('codes').where(ctx.db.raw('id > 0')).then((res: any) => {
+        await ctx.db('codes').where(ctx.db.raw('id > 0')).limit(10).offset(30).then((res: any) => {
             ctx.body = {
                 a: 1,
                 b: 2,
